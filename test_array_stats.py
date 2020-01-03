473fd2f66fef25b421908a5bc6d9a148f410287e
noissefnoc <noissefnoc@gmail.com>
2020-01-03 14:22:06 +0900

    finished implement test_array_stats.py

--- test_array_stats.py
+++ test_array_stats.py
@@ -6,40 +6,67 @@ from numpy.testing import assert_array_equal
 class TestArrayStats(unittest.TestCase):
 
     def test_sum(self):
+        """
+        合計値
+        """
         metrix = np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]])
-        self.assertEqual(np.sum(metrix), x)
+        self.assertEqual(np.sum(metrix), 45)
 
     def test_max(self):
+        """
+        最大値
+        """
         metrix = np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]])
-        self.assertEqual(np.max(metrix), x)
+        self.assertEqual(np.max(metrix), 9)
 
     def test_min(self):
+        """
+        最小値
+        """
         metrix = np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]])
-        self.assertEqual(np.min(metrix), x)
+        self.assertEqual(np.min(metrix), 0)
 
     def test_sum_axis0(self):
+        """
+        縦方向での合計
+        """
         metrix = np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]])
-        assert_array_equal(np.sum(metrix, axis=0), np.array([x, x, x, x, x]))
+        assert_array_equal(np.sum(metrix, axis=0), np.array([5, 7, 9, 11, 13]))
 
     def test_sum_axis1(self):
+        """
+        横方向での合計
+        """
         metrix = np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]])
-        assert_array_equal(np.sum(metrix, axis=1), np.array([x, x]))
+        assert_array_equal(np.sum(metrix, axis=1), np.array([10, 35]))
 
     def test_max_axis0(self):
+        """
+        縦方向の最大値
+        """
         metrix = np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]])
-        assert_array_equal(np.max(metrix, axis=0), np.array([x, x, x, x, x]))
+        assert_array_equal(np.max(metrix, axis=0), np.array([5, 6, 7, 8, 9]))
 
     def test_max_axis1(self):
+        """
+        横方向の最大値
+        """
         metrix = np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]])
-        assert_array_equal(np.max(metrix, axis=1), np.array([x, x]))
+        assert_array_equal(np.max(metrix, axis=1), np.array([4, 9]))
 
     def test_min_axis0(self):
+        """
+        縦方向の最小値
+        """
         metrix = np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]])
-        assert_array_equal(np.min(metrix, axis=0), np.array([x, x, x, x, x]))
+        assert_array_equal(np.min(metrix, axis=0), np.array([0, 1, 2, 3, 4]))
 
     def test_min_axis1(self):
+        """
+        横方向の最小値
+        """
         metrix = np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]])
-        assert_array_equal(np.min(metrix, axis=1), np.array([x, x]))
+        assert_array_equal(np.min(metrix, axis=1), np.array([0, 5]))
 
 
 if __name__ == '__main__':
