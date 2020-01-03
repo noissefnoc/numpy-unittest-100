6cd5a65e8acba2dcd50f593e81bf2092cfa7f0c3
noissefnoc <noissefnoc@gmail.com>
2020-01-03 14:08:47 +0900

    finished implement test_array_select.py

--- test_array_select.py
+++ test_array_select.py
@@ -6,14 +6,20 @@ from numpy.testing import assert_array_equal
 class TestArraySelect(unittest.TestCase):
 
     def test_select_cond1(self):
+        """
+        select: 条件式がTrueになった部分に演算を適応。Falseだと0
+        """
         vector = np.arange(10)
         vector = np.select([5 < vector], [vector ** 2])
-        assert_array_equal(vector, np.array([x, x, x, x, x, x, x, x, x, x]))
+        assert_array_equal(vector, np.array([0, 0, 0, 0, 0, 0, 36, 49, 64, 81]))
 
     def test_select_cond2(self):
+        """
+        select: 条件複数指定されている場合は、適応も配列の順番
+        """
         vector = np.arange(10)
         vector = np.select([vector < 3, 5 < vector], [vector, vector ** 2])
-        assert_array_equal(vector, np.array([x, x, x, x, x, x, x, x, x, x]))
+        assert_array_equal(vector, np.array([0, 1, 2, 0, 0, 0, 36, 49, 64, 81]))
 
 
 if __name__ == '__main__':
