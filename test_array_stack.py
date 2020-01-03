9326af6358f27cf80af36f5bdb4f5766a34f3e98
noissefnoc <noissefnoc@gmail.com>
2020-01-03 14:18:18 +0900

    finished implement test_array_stack.py

--- test_array_stack.py
+++ test_array_stack.py
@@ -6,22 +6,28 @@ from numpy.testing import assert_array_equal
 class TestArrayStack(unittest.TestCase):
 
     def test_vstack(self):
+        """
+        垂直に結合
+        """
         metrixA = np.array([[1, 2], [3, 4]])
         metrixB = np.array([[5, 6], [7, 8]])
         metrixC = np.vstack((metrixA, metrixB))
         assert_array_equal(metrixC,
-                          np.array([[x, x],
-                                    [x, x],
-                                    [x, x],
-                                    [x, x]]))
+                          np.array([[1, 2],
+                                    [3, 4],
+                                    [5, 6],
+                                    [7, 8]]))
 
     def test_hstack(self):
+        """
+        水平に結合
+        """
         metrixA = np.array([[1, 2], [3, 4]])
         metrixB = np.array([[5, 6], [7, 8]])
         metrixC = np.hstack((metrixA, metrixB))
         assert_array_equal(metrixC,
-                          np.array([[x, x, x, x],
-                                    [x, x, x, x]]))
+                          np.array([[1, 2, 5, 6],
+                                    [3, 4, 7, 8]]))
 
 
 if __name__ == '__main__':
